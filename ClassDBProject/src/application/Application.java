8fea95208a1cd4e9246ea916a4c66f233529d90d
Lisa Rodrigues <lisa.r.rodrigues@gmail.com>
2020-12-11 15:51:19 -0800

    huzzah

--- ClassDBProject/src/application/Application.java
+++ ClassDBProject/src/application/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Can you read this?");
+		System.out.println("Sure can!");
 	}
 
 }
