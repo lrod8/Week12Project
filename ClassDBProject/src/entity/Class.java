3289442e082b1d611ff028203a85f6c3bf69c803
Lisa Rodrigues <lisa.r.rodrigues@gmail.com>
2020-12-11 16:31:44 -0800

    updated class entity

--- ClassDBProject/src/entity/Class.java
+++ ClassDBProject/src/entity/Class.java
@@ -1,5 +1,27 @@
 package entity;
 
 public class Class {
+	
+	private int classId;
+	private String className;
+	
+	public Class(int classId, int teacherId, int studentId, String className) {
+		this.setClassId(classId);
+		this.setClassName(className);
+	}
+	
+	public int getClassId() {
+		return classId;
+	}
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+	
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
 
 }
