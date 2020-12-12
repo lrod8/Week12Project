874e0c51774f738945c0630d151da2ded3eb7a15
John <john.jdh55@gmail.com>
2020-12-11 22:06:10 -0800

    here is my Class change

--- ClassDBProject/src/entity/Class.java
+++ ClassDBProject/src/entity/Class.java
@@ -1,13 +1,19 @@
 package entity;
 
+import java.util.List;
+
 public class Class {
 	
 	private int classId;
 	private String className;
+	private List<Student> students;
+	private Teacher teachers;
 	
-	public Class(int classId, int teacherId, int studentId, String className) {
+	public Class(int classId, String className, List<Student> students, Teacher teachers) {
 		this.setClassId(classId);
 		this.setClassName(className);
+		this.setStudents(students);
+		this.setTeachers(teachers);
 	}
 	
 	public int getClassId() {
@@ -22,6 +28,22 @@ public class Class {
 	}
 	public void setClassName(String className) {
 		this.className = className;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	public Teacher getTeachers() {
+		return teachers;
+	}
+
+	public void setTeachers(Teacher teachers) {
+		this.teachers = teachers;
 	}
 
 }
