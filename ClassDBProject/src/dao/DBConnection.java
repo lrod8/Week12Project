eec8ccc5a9aa10d1dfa875efcf92b060c7f35006
Lisa Rodrigues <lisa.r.rodrigues@gmail.com>
2020-12-11 16:42:00 -0800

    add dao code

--- ClassDBProject/src/dao/DBConnection.java
+++ ClassDBProject/src/dao/DBConnection.java
@@ -1,5 +1,33 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+	
+	private final static String USERNAME = "week12_admin";
+	private final static String PASSWORD = "promineo12";
+	private final static String URL = "jdbc:mysql://week12schooldb.c6enpaa8xoep.us-west-1.rds.amazonaws.com:3306/school";
+	private static Connection connection;
+	private static DBConnection instance;
+	
+	@SuppressWarnings("static-access")
+	private DBConnection(Connection connection) {
+		this.connection = connection;
+	}
+			
+	public static Connection getConnection() {
+		if (instance == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				instance = new DBConnection(connection);
+				System.out.println("Connection successful!");
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return DBConnection.connection;
+	}
 
 }
--- ClassDBProject/src/dao/StudentDao.java
+++ ClassDBProject/src/dao/StudentDao.java
@@ -1,5 +1,17 @@
 package dao;
 
+import java.sql.Connection;
+
 public class StudentDao {
+	
+private Connection connection;
+	
+	//Insert The MYSQL private Final Strings in here
+	
+	public StudentDao() {
+		connection = DBConnection.getConnection();
+	}
+
+	//finish coding and setting the prepared statements
 
 }
