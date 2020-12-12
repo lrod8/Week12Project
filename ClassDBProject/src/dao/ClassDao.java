790db9164f74ad44f5675fbc568e19583177ecd8
Naida <ms_naida@hotmail.com>
2020-12-12 06:18:23 -0800

    add read to ClassDao

--- ClassDBProject/src/dao/ClassDao.java
+++ ClassDBProject/src/dao/ClassDao.java
@@ -2,7 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassDao {
 	
@@ -10,6 +13,8 @@ public class ClassDao {
 	
 	//Insert The MYSQL private Final Strings in here
 	private final String CREATE_NEW_CLASS_QUERY = "INSERT INTO class(className) VALUES (?)";
+	private final String GET_CLASS_QUERY = "SELECT * FROM class";
+	private final String GET_CLASS_BY_ID_QUERY = "SELECT * FROM classes WHERE id = ?";	
 	
 	public ClassDao() {
 		connection = DBConnection.getConnection();
@@ -21,6 +26,29 @@ public class ClassDao {
 		PreparedStatement ps = connection.prepareStatement(CREATE_NEW_CLASS_QUERY);
 		ps.setString(1, className);
 		ps.executeUpdate();
+	}
+	
+	public List<Class> getClasses() throws SQLException {	
+		ResultSet rs = connection.prepareStatement(GET_CLASS_QUERY).executeQuery();
+		List<Class> classes = new ArrayList<Class> ();
+		
+		while (rs.next()) {	
+			classes.add(populateClass(rs.getInt(1), rs.getString(2)));
+	}
+	
+		return classes;
+}
+	
+	public Class getClassById(int id) throws SQLException { 	
+		PreparedStatement ps = connection.prepareStatement(GET_CLASS_BY_ID_QUERY);
+		ps.setInt(1, id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();	
+		return populateClass(rs.getInt(1), rs.getString(2));	
+	}
+
+	private Class populateClass(int id, String name) throws SQLException {	
+		return new Class(id, name, studentDao.getstudentByClassId(id) + teacherDao.getteacherByClassId(id));
 	}
 
 }
